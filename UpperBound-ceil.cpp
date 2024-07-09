cd192c0fc54f21249a815ef13269f4703a76dbdc
SUSHANTH R HEGDE <120232856+SushanthRHegde@users.noreply.github.com>
2024-07-09 10:52:27 +0530

    Create UpperBound-ceil.cpp

---
+++ UpperBound-ceil.cpp
@@ -0,0 +1,14 @@
+int upperBound(vector<int> &arr, int x, int n){
+	// Write your code here.
+	int low = 0 , high = arr.size() - 1;
+	int ans = n;
+	while(low <= high)	{
+		int mid = (low + high)/2;
+		if(arr[mid] > x){
+			ans = mid;
+			high = mid - 1;
+		}
+		else low = mid + 1;
+	}
+	return ans;
+}
